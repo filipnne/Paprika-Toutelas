5c5df7d020fcf890c2df8cd433eeb4e09bdd5bd3
MIT AfriMalin <philip.ngimbis@afrimalin-group.com>
2018-08-07 17:55:29 +0100

    Création de la classe de base

--- tests/ToolsObjectBoxTest.php
+++ tests/ToolsObjectBoxTest.php
@@ -1,18 +1,18 @@
 <?php
 
-require_once __DIR__ . '/../WordWrap.php';
+require_once __DIR__ . '/../ToolsObjectBox.php';
  
 class ToolsObjectBoxTest extends PHPUnit_Framework_TestCase {
  
     function testItCanWrap() {
-        $w = new WordWrap();
+        $wordwrap = new ToolsObjectBox();
  
-        $this->assertEquals('', $w->wrap(null, 0));
-        $this->assertEquals('', $w->wrap('', 0));
-        $this->assertEquals('a', $w->wrap('a', 1));
-        $this->assertEquals("a\nb", $w->wrap('a b', 1));
-        $this->assertEquals("a b\nc", $w->wrap('a b c', 3));
-        $this->assertEquals("a\nbc\nd", $w->wrap('a bc d', 3));
+        $this->assertEquals('', $wordwrap->wrap(null, 0));
+        $this->assertEquals('', $wordwrap->wrap('', 0));
+        $this->assertEquals('a', $wordwrap->wrap('a', 1));
+        $this->assertEquals("a\nb", $wordwrap->wrap('a b', 1));
+        $this->assertEquals("a b\nc", $wordwrap->wrap('a b c', 3));
+        $this->assertEquals("a\nbc\nd", $wordwrap->wrap('a bc d', 3));
     }
 }
 
